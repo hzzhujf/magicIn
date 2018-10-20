9015e6c6a3d040531bd5da9e35969a6f6f68765b
Zhu Jiefeng <flyingpig@videopls.com>
2018-10-20 17:18:11 +0800

    demo

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, request
 from pathlib import Path
 import os
+import subprocess as sp
+import logging
+FFMPEG_BIN = '/usr/local/bin/ffmpeg'
 
 app = Flask(__name__)
 
@@ -17,14 +20,82 @@ def list():
 
   list = []
   pd = Path('static/video')
-  for childDir in pd.iterdir():
-    if childDir.is_dir():
-      pf = Path(os.path.join('static/video', childDir.name))
-      for childFile in pf.iterdir():
-        if not childFile.is_dir():
-          if childFile.name.find(childDir.name) != -1:
-            list.append(Video(childFile.name, os.path.join('static/video', childDir.name, childFile.name)))
+  for child_dir in pd.iterdir():
+    if child_dir.is_dir():
+      pf = Path(os.path.join('static/video', child_dir.name))
+      for child_file in pf.iterdir():
+        if not child_file.is_dir():
+          if child_file.name.find(child_dir.name) != -1:
+            list.append(Video(child_file.name, os.path.join('static/video', child_dir.name, child_file.name)))
             break
   return json.dumps(list, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
+@app.route('/api/ad', methods=['POST'])
+def ad():
+  file_name = request.form['video']
+  dir_name = 'static/video/%s' % file_name.split('.')[0]
+  seek_time_str = request.form['ss']
+  file = request.files['ad_layer']
+  file.save(os.path.join(dir_name, file.filename))
+  # 分割视频
+  # ffmpeg -y -i no_cover.mp4 -t 00:00:00.480 -c:v h264 -c:a aac start.mp4
+  divide_start_command = [FFMPEG_BIN,
+    '-y',
+    '-i', os.path.join(dir_name, file_name),
+    '-t', seek_time_str,
+    '-c:v', 'h264',
+    '-c:a', 'aac',
+    os.path.join(dir_name, 'start.mp4')]
+  dsp = sp.run(divide_start_command, capture_output=True)
+  if dsp.returncode != 0:
+    logging.error(dsp.stderr.decode('ascii'))
+    return 'error'
+  # ffmpeg -y -accurate_seek -ss 00:00:00.480 -i no_cover.mp4 -c:v h264 -c:a aac end.mp4
+  divide_end_command = [FFMPEG_BIN,
+    '-y',
+    '-accurate_seek', '-ss', seek_time_str,
+    '-i', os.path.join(dir_name, file_name),
+    '-c:v', 'h264',
+    '-c:a', 'aac',
+    os.path.join(dir_name, 'end.mp4')]
+  dep = sp.run(divide_end_command, capture_output=True)
+  if dep.returncode != 0:
+    logging.error(dep.stderr.decode('ascii'))
+    return 'error'
+  # 合成广告视频
+  # ffmpeg -y -i end.mp4 -i ad_layer.mov -filter_complex 'overlay' merged_end.mp4
+  merge_ad_command = [FFMPEG_BIN,
+    '-y',
+    '-i', '%s/end.mp4' % dir_name,
+    '-i', os.path.join(dir_name, file.filename),
+    '-filter_complex',
+    'overlay',
+    os.path.join(dir_name, 'merged_end.mp4')
+  ]
+  mp = sp.run(merge_ad_command, capture_output=True)
+  if mp.returncode != 0:
+    logging.error(mp.stderr.decode('ascii'))
+    return 'error'
+  # 合并最终预览视频
+  # ffmpeg -y -i input1.mp4 -i input2.webm -i input3.avi -filter_complex '[0:0] [0:1] [1:0] [1:1] [2:0] [2:1] concat=n=3:v=1:a=1 [v] [a]' -map '[v]' -map '[a]' <编码器选项> output.mkv
+  merge_result_command = [FFMPEG_BIN,
+    '-y',
+    '-i', os.path.join(dir_name, 'start.mp4'),
+    '-i', os.path.join(dir_name, 'merged_end.mp4'),
+    '-filter_complex', '[0:0] [0:1] [1:0] [1:1] concat=n=2:v=1:a=1 [v] [a]',
+    '-map', '[v]',
+    '-map', '[a]',
+    '-c:v', 'h264',
+    '-c:a', 'aac',
+    os.path.join(dir_name, 'result.mp4')
+  ]
+  mrp = sp.run(merge_result_command, capture_output=True)
+  if mrp.returncode != 0:
+    logging.error(mrp.stderr.decode('ascii'))
+    return 'error'
+  # 删除中间状态的临时视频
+  os.remove(os.path.join(dir_name, 'start.mp4'))
+  os.remove(os.path.join(dir_name, 'end.mp4'))
+  os.remove(os.path.join(dir_name, 'merged_end.mp4'))
 
+  return os.path.join(dir_name, 'result.mp4')
